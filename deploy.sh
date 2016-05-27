9bbf51e477c51acab17fc76bc8cca627089e5a43
gerry <weirich@elexis.ch>
2016-05-27 21:14:06 +0200

    deploayment script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-PRODUCTS=/var/www/vhosts/elexis.ch/httpdocs/ungrad/products/${BUILD_NUMBER}
+PRODUCTS=/var/www/vhosts/elexis.ch/httpdocs/ungrad/products/lucinda/${BUILD_NUMBER}
 P2=/var/www/vhosts/elexis.ch/httpdocs/ungrad/p2/lucinda/${BUILD_NUMBER}
 
-mkdir $PRODUCTS
-mkdir $P2
+mkdir -p $PRODUCTS
+mkdir -p $P2
 
 cp lucinda-server/target/lucinda-server*.jar $PRODUCTS
-cp lucinda-client/target/lucinda-client*.jar
+cp lucinda-client/target/lucinda-client*.jar $PRODUCTS
 cp -R elexis-docmgr-lucinda-p2/target/repository/* $P2
