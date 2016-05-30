ca1139fe1d03005705f97ea624fbc52d158503c6
gerry <weirich@elexis.ch>
2016-05-30 10:49:19 +0200

    bugfix

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-WEBSPACE=/var/www/vhosts/elexis.ch/httpdocs/ungrad/
+WEBSPACE=/var/www/vhosts/elexis.ch/httpdocs/ungrad
 
-PRODUCTS=${WEBSPACE}products/lucinda/${BUILD_NUMBER}
-P2=${WEBSPACE}p2/lucinda/${BUILD_NUMBER}
+PRODUCTS=${WEBSPACE}/products/lucinda
+P2=${WEBSPACE}/p2/lucinda
 
-mkdir -p $PRODUCTS
-mkdir -p $P2
+mkdir -p ${PRODUCTS}/${BUILD_NUMBER}
+mkdir -p ${P2}/${BUILD_NUMBER}
 
-cp lucinda-server/target/lucinda-server*.jar $PRODUCTS
-cp lucinda-client/target/lucinda-client*.jar $PRODUCTS
-cp -R elexis-docmgr-lucinda-p2/target/repository/* $P2
+cp lucinda-server/target/lucinda-server*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp lucinda-client/target/lucinda-client*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp -R elexis-docmgr-lucinda-p2/target/repository/* ${P2}/${BUILD_NUMBER}
 
 rm ${PRODUCTS}/latest
-ln -s ${BUILD_NUMBER} ${WEBSPACE}products/lucinda/latest
+ln -s ${BUILD_NUMBER} ${WEBSPACE}/products/lucinda/latest
 rm ${P2}/latest
-ln -s ${BUILD_NUMBER} ${WEBSPACE}p2/lucinda/latest
+ln -s ${BUILD_NUMBER} ${WEBSPACE}/p2/lucinda/latest
