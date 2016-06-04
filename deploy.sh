24c17271205c47d2b37d52c253f2a11e9741132b
gerry <weirich@elexis.ch>
2016-06-04 21:32:09 +0200

    refactoring of layout

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,11 @@
 WEBSPACE=/var/www/vhosts/elexis.ch/httpdocs/ungrad
 
 PRODUCTS=${WEBSPACE}/products/lucinda
-P2=${WEBSPACE}/p2/lucinda
 
 mkdir -p ${PRODUCTS}/${BUILD_NUMBER}
-mkdir -p ${P2}/${BUILD_NUMBER}
 
 cp lucinda-server/target/lucinda-server*.jar ${PRODUCTS}/${BUILD_NUMBER}
 cp lucinda-client/target/lucinda-client*.jar ${PRODUCTS}/${BUILD_NUMBER}
-cp -R elexis-docmgr-lucinda-p2/target/repository/* ${P2}/${BUILD_NUMBER}
 
 rm ${PRODUCTS}/latest
 ln -s ${BUILD_NUMBER} ${WEBSPACE}/products/lucinda/latest
-rm ${P2}/latest
-ln -s ${BUILD_NUMBER} ${WEBSPACE}/p2/lucinda/latest
