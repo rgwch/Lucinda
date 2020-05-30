11fd5090e9a102e3a446407f1251a12cbd4ccedc
rgwch <rgw@elexis.ch>
2020-05-30 08:12:31 +0200

    ocrmypdf

--- server/src/importer.js
+++ server/src/importer.js
@@ -1,29 +1,61 @@
 const { parentPort, workerData } = require('worker_threads')
 const cfg = require('config')
+if (!cfg.has('solr')) {
+  throw new Error("Solr is not defined in the config")
+}
+if (!cfg.has('tika')) {
+  throw new Error("Tika is not defined in the config")
+}
+
 const log = require('./logger')
 const { spawn } = require('child_process')
 const fs = require('fs').promises
 const path = require('path')
 const { versionsPath } = require('./files')
+const fetch = require('node-fetch')
+
+const getTikaURL = tika => `${tika.host}:${tika.port}/`
+const getMetaURL = () => getTikaURL(cfg.get("tika")) + "meta"
+const getContentsURL = () => getTikaURL(cfg.get("tika")) + "tika"
+const getSolrURL = solr => `${solr.host}:${solr.port}/`
+
 
 if (workerData) {
-  log.info("received job " + workerData)
-  const dest = versionsPath() + path.sep + createVersion(workerData)
-  if (workerData.endsWith("pdf")) {
-    doOCR(workerData, dest).then(async dest => {
+  doImport(workerData).then(result => {
+    parentPort.postMessage(result)
+  })
+}
+
+async function doImport(filename) {
+  try {
+    const hasTika = await fetch(getTikaURL(cfg.get("tika")))
+    if (hasTika.status !== 200) {
+      throw new Error("Could not access Tika")
+    }
+    const hasSolr = await fetch(getSolrURL(cfg.get("solr")))
+    if (hasSolr.status !== 200) {
+      throw new Error("Could not access Solr")
+    }
+  } catch (ex) {
+    log.error(ex)
+    throw (ex)
+  }
+  log.info("received job " + filename)
+  const dest = versionsPath() + path.sep + createVersion(filename)
+  if (filename.endsWith("pdf")) {
+    doOCR(filename, dest).then(async dest => {
       const stat = await fs.stat(dest)
       if (stat.isFile() && stat.size) {
-        const buf = await fs.readFile(workerData)
-        await fs.copyFile(dest, workerData)
+        const buf = await fs.readFile(filename)
+        await fs.copyFile(dest, filename)
         await fs.unlink(dest)
         await fs.writeFile(dest, buf)
       }
-      parentPort.postMessage(workerData)
     })
-  } else {
-    parentPort.postMessage(workerData)
   }
 }
+
+
 
 function createVersion(fn) {
   const dat = new Date()
@@ -49,4 +81,4 @@ function doOCR(source, dest) {
   })
 }
 
-module.exports = doOCR
+module.exports = { doImport }
--- server/test/integration/test.js
+++ server/test/integration/test.js
@@ -1,23 +1,10 @@
 const fetch = require('node-fetch')
 const fs = require('fs').promises
-const path=require('path')
+const path = require('path')
+const importer = require('../../src/importer')
 
-get().then(ok => {
-    console.log("ok")
-}).catch(err=>{
-    console.log(err)
+
+const dir = path.join(__dirname, "../")
+fs.copyFile(dir + "pdf_image_test_orig.pdf", dir + "pdf_image_test.pdf").then(res => {
+  importer.doImport(dir + "pdf_image_test.pdf")
 })
-
-async function get() {
-    const cnt = await fs.readFile(path.join(__dirname,"testocr.pdf"))
-
-    result = await fetch("http://localhost:3030",
-        {
-            headers: { "content-type": "application/octet-stream", accept: "application/octet-stream" },
-            method: "post",
-            body: cnt
-        })
-    const file=await result.buffer()
-    await fs.writeFile(path.join(__dirname,"testresult.pdf"), file)
-    return "ok"
-}
