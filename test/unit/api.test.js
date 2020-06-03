70aaa64c2e712d7997d4dbdae623e9830de6d64c
rgwch <weirich@elexis.ch>
2020-06-03 20:11:36 +0200

    added api test

---
+++ test/unit/api.test.js
@@ -0,0 +1,53 @@
+
+process.env.NODE_ENV = "test"
+const chai = require('chai')
+chai.should()
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp)
+const fs = require('fs')
+const path = require('path')
+const API = "/lucinda/3.0"
+
+describe("lucinda API", () => {
+  const server = require('../../src/server')
+
+  it("responses to ping", done => {
+    chai.request(server)
+      .get(API + "/")
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.an('object')
+        res.body.status.endsWith("ok").should.be.true
+        done()
+      })
+  })
+
+  it("queries for files", () => {
+    chai.request(server)
+      .get(API + "/query/*")
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.an("object")
+        res.body.should.have.property('numFound')
+        res.body.should.have.property('docs')
+      })
+  })
+  it("stores a file", () => {
+    const file = fs.readFileSync(path.join(__dirname, "../lorem.pdf.text"))
+    chai.request(server)
+      .post(API + "/add")
+      .set('content-type', 'application/json')
+      .send({
+        metadata: {
+          filepath: "/testperson/lorem.pdf",
+        },
+        payload: file.toString()
+      })
+      
+      .end((err, res) => {
+        res.should.have.status(202)
+      })
+
+
+  })
+})
