0b8a94f5e20d0f6d27cf664750e6bf0b279c0949
rgwch <weirich@elexis.ch>
2020-06-07 17:15:37 +0200

    removed v2

--- lucinda-client/readme.md
+++
@@ -1,4 +0,0 @@
-# Lucinda Client
-
-A simple client application library for the Lucinda server. Removes the need
-to handle messaging and binding individually to vertx-related libraries.  
--- lucinda-server/rundocker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# run like: ./rundocker.sh full-path-to-documents
-
-docker rm lucinda
-docker run -p 2016:2016 --name lucinda -v lucinda_home:/var/lucinda/home -v $1:/var/lucinda/docs rgwch/lucinda-server:`cat VERSION`
